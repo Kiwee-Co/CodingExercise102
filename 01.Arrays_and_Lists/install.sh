a732e811d40e32f0df012a59479a023739b12262
Jerry Yang <jerry@chenyang.us>
2024-01-19 18:07:35 -0800

    check out class materials

---
+++ 01.Arrays_and_Lists/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+brew install git maven sourcetree maven-completion eclipse-jee -y 
+
+echo "checking out the materials.."
+
+# setup git repo
+mkdir -p ~/Projects/kiwee-co/
+cd ~/Projects/kiwee-co/
+git clone https://github.com/Kiwee-Co/Coding101.git
+git clone https://github.com/Kiwee-Co/CodingExercise101.git
+
+echo "github materials installed!"
+brew cleanup -s --prune=all 
+
+echo "All done!"
+
