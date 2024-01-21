8c19ff173c20613ae572347b7a859bf69d1dedfa
Jerry Yang <jerry@chenyang.us>
2024-01-20 18:30:01 -0800

    Update install.sh

--- 01.Arrays_and_Lists/install.sh
+++ 01.Arrays_and_Lists/install.sh
@@ -7,8 +7,8 @@ echo "checking out the materials.."
 # setup git repo
 mkdir -p ~/Projects/kiwee-co/
 cd ~/Projects/kiwee-co/
-git clone https://github.com/Kiwee-Co/Coding101.git
-git clone https://github.com/Kiwee-Co/CodingExercise101.git
+git clone https://github.com/Kiwee-Co/Coding102.git
+git clone https://github.com/Kiwee-Co/CodingExercise102.git
 
 echo "github materials installed!"
 brew cleanup -s --prune=all 
